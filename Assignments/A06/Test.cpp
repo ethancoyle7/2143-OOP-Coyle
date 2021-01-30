a75cd91fa3c30dc8487b9c75b2cf533d769d0223
ethancoyle7 <77364000+ethancoyle7@users.noreply.github.com>
2021-01-29 18:47:35 -0600

    Update Test.cpp

--- Assignments/A06/Test.cpp
+++ Assignments/A06/Test.cpp
@@ -1,27 +1,28 @@
-
-
-
 #include <SFML/Graphics.hpp>
 
-int main() 
+int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+	sf::RenderWindow window(sf::VideoMode(500, 400), "SFML works! Ethan Coyle");
+	//controls the size of the black box encompassing the green circle
+	// can type different numbers inside of the parameters window size
 
-    while (window.isOpen()) 
-     {
-      sf::Event event;
-      while (window.pollEvent(event)) 
-      {
-            if (event.type == sf::Event::Closed)
-                window.close();
-      }
+	// controls size of green the " . f"
+	sf::CircleShape shape(200.f);
 
-      window.clear();
-      window.draw(shape);
-      window.display();
-    }
+	//can change to different color
+	shape.setFillColor(sf::Color::Green); 
 
-    return 0;
-}
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))// while it is in event
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();// if the event isnt running then close
+		}
+
+		window.clear();
+		window.draw(shape);// draws the shape
+		window.display();// display the circle
+		
+	}
